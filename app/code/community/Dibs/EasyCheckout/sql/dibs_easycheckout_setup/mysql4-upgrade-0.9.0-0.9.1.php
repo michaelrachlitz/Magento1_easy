f4059c441613b0a32ba31c50c9ec6c17bfca5f86
lenoid <leonid.novikov@vaimo.com>
2017-07-19 10:40:36 +0300

    Fixed Order Payment ID issue

---
+++ app/code/community/Dibs/EasyCheckout/sql/dibs_easycheckout_setup/mysql4-upgrade-0.9.0-0.9.1.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) 2009-2017 Vaimo Group
+ *
+ * Vaimo reserves all rights in the Program as delivered. The Program
+ * or any portion thereof may not be reproduced in any form whatsoever without
+ * the written consent of Vaimo, except as provided by licence. A licence
+ * under Vaimo's rights in the Program may be available directly from
+ * Vaimo.
+ *
+ * Disclaimer:
+ * THIS NOTICE MAY NOT BE REMOVED FROM THE PROGRAM BY ANY USER THEREOF.
+ * THE PROGRAM IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE PROGRAM OR THE USE OR OTHER DEALINGS
+ * IN THE PROGRAM.
+ *
+ * @category    Dibs
+ * @package     Dibs_EasyCheckout
+ * @copyright   Copyright (c) 2009-2017 Vaimo Group
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$connection = $installer->getConnection();
+
+$orderTable = $installer->getTable('sales_flat_order');
+
+$connection->modifyColumn($orderTable, 'dibs_easy_payment_id',
+    array(
+        'type'=>Varien_Db_Ddl_Table::TYPE_TEXT,
+        'length' => '255',
+        'unsigned' => true,
+        'nullable' => true,
+        'default' => NULL,
+        'comment' => 'Dibs Easy Payment Id'
+    )
+);
+
+$quoteTable = $installer->getTable('sales_flat_quote');
+$connection->modifyColumn($quoteTable, 'dibs_easy_grand_total',
+    array(
+        'type'=>Varien_Db_Ddl_Table::TYPE_DECIMAL,
+        'scale'     => 4,
+        'precision' => 12,
+        'length' => '255',
+        'unsigned' => true,
+        'nullable' => true,
+        'default' => NULL,
+        'comment' => 'Dibs Easy Payment Id'
+    )
+);
+
+
+
+$installer->endSetup();
